8fa571249d3b1f9fb65c87bb4badc4a0c0fe71e9
Jason Welsh <welshjason03@gmail.com>
2025-07-28 14:07:15 -0400

    Pushing Controller updates.

--- FinalProject1/Controllers/TeamsController.cs
+++ FinalProject1/Controllers/TeamsController.cs
@@ -1,10 +1,68 @@
+using FinalProject1.DTO;
+using FinalProject1.Models;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 namespace FinalProject1.Controllers;
 
 
-
+[ApiController]
+[Route("[controller]")]
 
 public class TeamsController : ControllerBase
 {
+    private readonly FinalProjectContext _db;
+
+    public TeamsController(FinalProjectContext db) {
+        _db = db;
+    }
+
+    [HttpGet("GetTeamsInformation")]
+    public ActionResult GetTeamMembers(int? Id) {
+
+        if (Id == null || Id == 0)
+        {
+            TeamsResponse team = new TeamsResponse();
+
+            var response = _db.Teams.AsNoTracking()
+                            .OrderByDescending(x => x.Id)
+                            .Take(5);
+
+            foreach (var item in response)
+            {
+                team.Id = item.Id;
+                team.TeamMember = item.TeamMember;
+                team.BirthDate = item.BirthDate;
+                team.CollegeProgram = item.CollegeProgram;
+                team.Year = item.Year;
+            }
+
+            return Ok(team);
+        }
+        else
+        {
+            var response = _db.Teams.AsNoTracking()
+                                    .First();
+            return Ok(response);
+        }
+
+    }
+    [HttpPut("CreateTeamMember")]
+    public void CreateTeamMember(string name, DateTime birthDate, string collegeProgram, string year)
+    {
+        Team teamMate = new Team();
+        teamMate.TeamMember = name;
+        teamMate.BirthDate = birthDate;
+        teamMate.CollegeProgram = collegeProgram;
+        teamMate.Year = year;
+        var response = _db.Teams.Add(teamMate);
+        _db.SaveChanges();
+    }
+
+    // [HttpDelete("DeleteTeamMember")]
+
+    // [HttpPut("UpdateTeamMember")]
 
 
 }
