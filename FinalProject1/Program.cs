8bfc3e147026264a7a799f20be987fd39692d535
williampinson <pinsonwd@mail.uc.edu>
2025-07-28 23:15:39 -0400

    Initial commit

--- FinalProject1/Program.cs
+++ FinalProject1/Program.cs
@@ -1,4 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using FinalProject1.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<FinalProject1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("FinalProject1Context") ?? throw new InvalidOperationException("Connection string 'FinalProject1Context' not found.")));
 
 // Add services to the container.
 
