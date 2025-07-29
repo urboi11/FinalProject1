db190614063090fc14fe80eacb40c83fbc4d5166
Jason Welsh <welshjason03@gmail.com>
2025-07-28 22:39:35 -0400

    Adding Best Friend Controller

--- FinalProject1/Migrations/20250729022710_BestFriendCreation.Designer.cs
+++ FinalProject1/Migrations/20250729022710_BestFriendCreation.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FinalProject1.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace FinalProject1.Migrations
 {
     [DbContext(typeof(FinalProjectContext))]
-    partial class FinalProjectContextModelSnapshot : ModelSnapshot
+    [Migration("20250729022710_BestFriendCreation")]
+    partial class BestFriendCreation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.18");
