93658c40827624e25ca09b64d5d4f82b62330eb0
Jason Welsh <welshjason03@gmail.com>
2025-08-04 13:54:25 -0400

    Revert "Commenting out Swagger UI in development mode to Production."
    This reverts commit 46cb6a98350f24b3b978bd6d5bf790ef2eae61ce.

--- FinalProject1/Program.cs
+++ FinalProject1/Program.cs
@@ -17,10 +17,11 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
